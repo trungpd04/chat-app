1aaacfb60aa774d083566263c01599b7e8d0c13f
crazyboy611 <129376844+crazyboy611@users.noreply.github.com>
2026-04-17 15:05:22 +0700

    refactor

---
+++ src/main/java/com/dtrung/chatapp/response/FriendRequestResponse.java
@@ -0,0 +1,28 @@
+package com.dtrung.chatapp.response;
+
+import com.dtrung.chatapp.model.FriendshipStatus;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class FriendRequestResponse {
+    UUID id;
+    UUID senderId;
+    String senderUsername;
+    String senderAvatar;
+    FriendshipStatus status;
+    LocalDateTime createdAt;
+}
